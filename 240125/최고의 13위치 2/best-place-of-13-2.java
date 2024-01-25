139e75c3cc5e21ca32e4823461fad514d8cefc98
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-01-25 23:31:40 +0900

    Title: 최고의 13위치 2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240125/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        int n = Integer.parseInt(N);
+
+        int[][] board = new int[n][n];
+
+        for(int i =0; i< n; i++){
+            String tmp = br.readLine();
+            String[] tmp_arr = tmp.split(" ");
+
+            for(int k =0; k< n; k++){
+                board[i][k] = Integer.parseInt(tmp_arr[k]);
+            }
+        }
+
+        int answer = 0;
+        for(int i =0; i< n; i++){
+            for(int k =0; k< n-2; k++){
+                
+
+
+                for(int j = i; j < n; j++){
+                    for(int l = k; l < n-2; l++){
+                        if(j == i & l < k + 2) continue;
+                        // System.out.println(i + " " + k + " : " + j + " " + l);
+                        int clac = board[i][k] + board[i][k+1] + board[i][k+2] + board[j][l] +  board[j][l+1] +  board[j][l+2];
+                        answer = Math.max(answer, clac);
+                    }
+                }
+
+
+            }
+        }
+
+        System.out.println(answer);
+         
+
+    }
+}
