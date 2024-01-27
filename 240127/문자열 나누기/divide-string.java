3f84d5f4c77050acd26f4c97d1d3e4872a1e2b27
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-01-27 15:35:09 +0900

    Title: 문자열 나누기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240127/문자열 나누기/divide-string.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String N = sc.nextLine();
+        int n = Integer.parseInt(N);
+
+        String tmp = sc.nextLine();
+        String[] tmp_arr = tmp.split(" ");
+        String answer ="";
+        for(int i =0; i<n; i++){
+            answer += tmp_arr[i];
+        }
+
+        for(int i = 0; i< answer.length()/5; i++){
+            for(int k =0; k < 5; k++){
+                System.out.print(answer.charAt(i*5 + k));
+            }
+            System.out.println();
+        }
+        System.out.println(answer.length()/5);
+    }
+}
