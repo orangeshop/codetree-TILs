475948ea7d1a39ccce55ee9819fd98a3fe91f120
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-01-27 14:18:17 +0900

    Title: 문자열 거꾸로 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240127/문자열 거꾸로 출력하기/print-string-backward.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            String tmp = sc.nextLine();
+            if("END".equals(tmp)) break;
+
+
+            for(int i = tmp.length()-1; i >= 0; i--){
+                System.out.print(tmp.charAt(i));
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
