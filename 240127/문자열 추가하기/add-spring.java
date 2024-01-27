b126f73f5e0ac1c5c0487f329913b86f45fcfa65
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-01-27 15:21:15 +0900

    Title: 문자열 추가하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240127/문자열 추가하기/add-spring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String tmp = sc.nextLine();
+
+        System.out.println(tmp+"Hello");
+    }
+}
