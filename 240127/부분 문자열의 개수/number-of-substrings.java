fe760b22655633844103e5deb98c4167a3318219
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-01-27 17:09:00 +0900

    Title: 부분 문자열의 개수, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240127/부분 문자열의 개수/number-of-substrings.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String tmp = sc.nextLine();
+        String tmp1 = sc.nextLine();
+        
+        int answer = 0;
+        for(int i =0; i< tmp.length()-tmp1.length()+1; i++){
+            String tmp2 = "";
+            for(int k = i; k < i+tmp1.length(); k++){
+                tmp2 += tmp.charAt(k);
+                // System.out.println(k);
+            }
+
+            // System.out.println(tmp2);
+
+            if(tmp2.equals(tmp1)){
+                answer++;
+                
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
