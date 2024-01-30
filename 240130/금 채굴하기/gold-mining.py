8aa0ea737f6ee24721dd006dfb2a8521536b4172
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-01-30 23:21:00 +0900

    Title: 금 채굴하기, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240130/금 채굴하기/gold-mining.py
@@ -0,0 +1,79 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+board = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    tmp = input().split()
+    for k in range(n):
+        board[i][k] = tmp[k]
+
+
+def clac(m, x,y, board, n):
+    cnt = [[-1 for _ in range(n)] for _ in range(n)]
+    vis = [[False for _ in range(n)] for _ in range(n)]
+    
+    Q = deque();
+    Q.append([x,y])
+    dx = [1,0,-1,0]
+    dy = [0,1,0,-1]
+    vis[x][y] = True
+    cnt[x][y] = 0
+    dig_cnt = m * m + (m + 1) * (m  + 1)
+    while(len(Q) != 0):
+        cur = Q.popleft()
+        # print(cur)
+        for i in range(4):
+            nx = cur[0] + dx[i]
+            ny = cur[1] + dy[i]
+
+            # print(f'{nx} {ny}')
+            if(nx < 0 or nx >= n or ny < 0 or ny >= n):
+                continue
+            if(vis[nx][ny] == True):
+                continue
+            if(cnt[cur[0]][cur[1]]+1 > m):
+                continue
+
+            
+            cnt[nx][ny] = cnt[cur[0]][cur[1]] + 1
+            Q.append([nx,ny])
+            vis[nx][ny] = True;
+
+    answer = 0
+    for i in range(n):
+        for k in range(n):
+            if(cnt[i][k] != -1):
+                answer += int(board[i][k])
+
+    # print(f"{x} {y} {answer}")
+    if(((5*answer) - dig_cnt ) > 0):
+        return answer
+    else:
+        return 0
+
+
+
+answer1 = 0;
+for i in range(n):
+    for k in range(n):
+        for j in range(0,3):
+            answer1 = max(clac(j,i,k,board,n), answer1)
+
+
+print(answer1)
+
+# 1
+
+# 0 1 0
+# 1 1 1
+# 0 1 0
+
+# 2
+
+# 0 0 1 0 0
+# 0 1 1 1 0
+# 1 1 1 1 1
+# 0 1 1 1 0
+# 0 0 1 0 0
