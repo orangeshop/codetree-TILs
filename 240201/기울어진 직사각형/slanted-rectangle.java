de80dc9c40c218f46fad5c0c73bb790bfd9303e5
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-02-01 23:57:14 +0900

    Title: 기울어진 직사각형, Time: 227ms, Memory: 17MB, Status: Passed - CodeTree

--- 240201/기울어진 직사각형/slanted-rectangle.java
+++ 240201/기울어진 직사각형/slanted-rectangle.java
@@ -1,94 +1,110 @@
 import java.util.*;
 import java.math.*;
 public class Main {
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
 
-    Scanner sc = new Scanner(System.in);
-    String N = sc.nextLine();
-    int n = Integer.parseInt(N);
+        Scanner sc = new Scanner(System.in);
+        String N = sc.nextLine();
+        int n = Integer.parseInt(N);
 
-    int[][] board = new int[n][n];
+        int[][] board = new int[n][n];
 
-    for(int i =0; i< n; i++){
-        String tmp = sc.nextLine();
-        String[] tmp_arr = tmp.split(" ");
-        for(int k =0; k < n; k++){
-            board[i][k] = Integer.parseInt(tmp_arr[k]);
-        }
-    }
-    int answer = 0;
-    for(int i =0; i< n; i++){
-        for(int k =1; k<n; k+=2){
-            // System.out.println(i + " " + k);
-
-            List<Pair> ls = new ArrayList<>();
-            ls.add(new Pair(i,k));
-            for(int j =0; j < n; j++){
-                int nx = ls.get(ls.size()-1).x - 1;
-                int ny = ls.get(ls.size()-1).y + 1;
-                
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n) break;
-
-                ls.add(new Pair(nx,ny));
+        for(int i =0; i< n; i++){
+            String tmp = sc.nextLine();
+            String[] tmp_arr = tmp.split(" ");
+            for(int k =0; k < n; k++){
+                board[i][k] = Integer.parseInt(tmp_arr[k]);
             }
-            int size = ls.size();
-            for(int j = 0; j < n; j++){
-                List<Pair> ls_tmp = new ArrayList<>();
-                boolean ch = false;
-                for(int l = 0; l < size; l++){
-                    int nx = ls.get((ls.size() - size) + l).x - 1;
-                    int ny = ls.get((ls.size() - size) + l).y - 1;
-                
-                    if(nx < 0 || nx >= n || ny < 0 || ny >= n) {
-                        ch = true;
-                        break;
-                    }
-                    ls_tmp.add(new Pair(nx,ny));
-
-                }
-
-                
-                if(ch == false){
-                    int tmp_result = 0;
-                    for(int l = 0; l < size; l++){
-                        tmp_result += board[ls_tmp.get(l).x][ls_tmp.get(l).y];
-                    }
-
-                    
-                    int tmp_ls_result = 0;
-                    for(int l = 0; l < size; l++){
-                        tmp_ls_result += board[ls.get(l).x][ls.get(l).y];
-                    }
-
-                    answer = Math.max(answer, tmp_result+tmp_ls_result);
-
-                    ls.add(ls_tmp.get(0));
-                    ls.add(ls_tmp.get(ls_tmp.size()-1));
-                    
-
-                }
-
-            }
-            
-            
-            
-            // System.out.println(answer);
-            // break;
         }
-        // break;
+        int answer = 0;
+
+        for(int i =0; i<n; i++){
+            for(int k =0; k < n; k++){
+                answer = Math.max(answer, clac(i,k,n,board));
+            }
+        }
 
         
-    }
         System.out.println(answer);
+
     }
 
-    static class Pair{
-        int x =0;
-        int y =0;
-        Pair(int x,int y){
-            this.x = x;
-            this.y = y;
+    static int clac(int x, int y, int max, int[][] board){
+        
+        int answer = 0;
+        for(int mr=1; mr <= max; mr++){
+            for(int mc = 1; mc <= max; mc++){
+                // System.out.println(mr + " " + mc);
+                answer = Math.max(answer, clac_sub(x, y, mr, mc, board, max));
+
+            }
         }
+        
+
+        return answer;
     }
+
+    // 0,0 0,1 0,2
+    // 1,0 1,1 1,2
+    // 2,0 2,1 2,2
+
+    static boolean ch(int nx, int ny, int n){
+        if(nx < 0 || nx >= n || ny < 0 || ny >= n){
+            return false;
+        }
+        return true;
+    }
+
+    static int clac_sub(int x, int y, int mr, int mc, int board[][], int n){
+        int dx[] = {-1,-1,1,1};
+        int dy[] = {1,-1,-1,1};
+        int nx = x;
+        int ny = y;
+        int sum = 0;
+        for(int i = 0; i < mr; i++){
+
+            nx += dx[0];
+            ny += dy[0];
+            if(ch(nx, ny, n) == false){
+                return -1;
+            }
+            sum += board[nx][ny];
+        }
+
+        for(int i = 0; i < mc; i++){
+
+            nx += dx[1];
+            ny += dy[1];
+            if(ch(nx, ny, n) == false){
+                return -1;
+            }
+            sum += board[nx][ny];
+        }
+
+        for(int i = 0; i < mr; i++){
+
+            nx += dx[2];
+            ny += dy[2];
+            if(ch(nx, ny, n) == false){
+                return -1;
+            }
+            sum += board[nx][ny];
+        }
+
+        for(int i = 0; i < mc; i++){
+
+            nx += dx[3];
+            ny += dy[3];
+            if(ch(nx, ny, n) == false){
+                return -1;
+            }
+            sum += board[nx][ny];
+        }
+
+
+        return sum;
+    }
+
 }
