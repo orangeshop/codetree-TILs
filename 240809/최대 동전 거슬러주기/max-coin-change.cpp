088b57fead72a2f79147bd15ef656787e0852a42
최영호 <81177568+orangeshop@users.noreply.github.com>
2024-08-09 00:04:06 +0900

    Title: 최대 동전 거슬러주기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/최대 동전 거슬러주기/max-coin-change.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+
+vector<int> V;
+
+int dp[10005];
+
+int main() {
+    
+    cin >> N >> M;
+
+    fill(dp, dp + 10005, -100);
+
+    for(int i =0; i < N; i++){
+        int a = 0;
+        cin >> a;
+        V.push_back(a);
+    }
+
+    dp[1] = 0;
+
+    for(int i =0; i < 10001; i++){
+        for(int k = 0; k < V.size(); k++){
+            if(i - V[k] > 0){
+                dp[i] = max(dp[i], dp[i - V[k]] + 1);
+            }
+        }
+    }
+    
+    if(dp[M] == -100){
+        cout << -1;
+    }else{
+        cout << dp[M];
+    }
+
+    return 0;
+}
