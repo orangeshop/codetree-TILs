974dc330fc2e2e177619d0eb4af23390dde6ebec
최영호 <81177568+orangeshop@users.noreply.github.com>
2024-08-08 23:23:26 +0900

    Title: 부분 수열의 합이 m, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> V;
+
+int dp[10005];
+
+int main() {
+    
+    cin >> N >> M;
+
+    fill(dp, dp + 10005, 1e9);
+
+    for(int i =0; i < N; i++){
+        int a = 0;
+        cin >> a;
+        V.push_back(a);
+    }
+
+    dp[0] = 0;
+
+    for(int i =0; i < V.size(); i++){
+        for(int k = 10000; k >=0; k--){
+            if(k - V[i] != 1e9){
+                // cout << k << " " << k - V[i] << endl;
+                dp[k] = min(dp[k], dp[k - V[i]] + 1);
+            }
+        }
+    }
+
+    // for(int i =0; i < 20; i++){
+    //     cout << dp[i] << endl;
+    // }
+
+    if(dp[M] == 1e9) cout << -1;
+    else cout << dp[M];
+
+
+
+
+    return 0;
+}
